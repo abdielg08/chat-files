079d4b475e1c78e477954279e40da3f59073d94b
abdielg08 <69168478+abdielg08@users.noreply.github.com>
2024-09-03 16:28:48 -0500

    Update constants.py

--- app/common/constants.py
+++ app/common/constants.py
@@ -5,4 +5,4 @@ from chromadb.config import Settings
 #PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', 'db')
 
 # Define the Chroma settings
-CHROMA_SETTINGS = chromadb.HttpClient(host="host.docker.internal", port=8000, settings=Settings(allow_reset=True, anonymized_telemetry=False))
+CHROMA_SETTINGS = chromadb.HttpClient(host="https://chroma-602716772659.us-central1.run.app", port=8000, settings=Settings(allow_reset=True, anonymized_telemetry=False))
